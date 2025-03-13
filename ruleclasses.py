9c9538b7243446550ee8560f33d52ef302065988
Kolja Kauder <kkauder@gmail.com>
2025-03-13 16:58:51 -0400

    slowly adding in cleaned up functionality

--- ruleclasses.py
+++ ruleclasses.py
@@ -1,9 +1,14 @@
 import yaml
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
 from typing import Dict, Any, Optional
 
+# Explicitly marking the dataclasses as mutable (frozen=False) to allow for
+# modification of the attributes after instantiation. 
+# After construction from a YAML file, the RuleConfig object may be modified
+# to add additional attributes, change existing ones, replace placeholders.
+
 # ============================================================================
-@dataclass
+@dataclass( frozen = False )
 class InputConfig:
     """Represents the input configuration block in the YAML."""
 
@@ -13,7 +18,13 @@ class InputConfig:
 
 
 # ============================================================================
-@dataclass
+_default_filesystem = {
+        'outdir'  :           "/sphenix/lustre01/sphnxpro/production/$(runname)/$(runtype)/$(build)_$(tag)_$(version)/{leafdir}/run_$(rungroup)/dst"
+    ,   'logdir'  : "file:///sphenix/data/data02/sphnxpro/production/$(runname)/$(runtype)/$(build)_$(tag)_$(version)/{leafdir}/run_$(rungroup)/log"
+    ,   'histdir' :        "/sphenix/data/data02/sphnxpro/production/$(runname)/$(runtype)/$(build)_$(tag)_$(version)/{leafdir}/run_$(rungroup)/hist"
+    ,   'condor'  :                                 "/tmp/production/$(runname)/$(runtype)/$(build)_$(tag)_$(version)/{leafdir}/run_$(rungroup)/log"    
+}
+@dataclass( frozen = False )
 class JobConfig:
     """Represents the job configuration block in the YAML."""
 
@@ -25,9 +36,11 @@ class JobConfig:
     priority: str
     request_xferslots: str
     batch_name: Optional[str] = None
+    filesystem: Optional[dict] = None
+
 
 # ============================================================================
-@dataclass
+@dataclass( frozen = False )
 class RuleConfig:
     """Represents a single rule configuration in the YAML."""
 
@@ -79,24 +92,37 @@ class RuleConfig:
         params_data = rule_data.get("params", {})
         required_params_fields = ["name", "build", "build_name", "dbtag", "version", "logbase", "outbase", "script",
                                   "payload", "neventsper", "comment", "rsync", "mem"]
-        for field in required_params_fields:
-            if field not in params_data:
-                raise ValueError(f"Missing required field '{field}' in params for rule '{rule_name}'.")
+        for f in required_params_fields:
+            if f not in params_data:
+                raise ValueError(f"Missing required field '{f}' in params for rule '{rule_name}'.")
 
         # Extract and validate input
         input_data = rule_data.get("input", {})
         required_input_fields = ["db", "query"]
-        for field in required_input_fields:
-            if field not in input_data:
-                raise ValueError(f"Missing required field '{field}' in input for rule '{rule_name}'.")
+        for f in required_input_fields:
+            if f not in input_data:
+                raise ValueError(f"Missing required field '{f}' in input for rule '{rule_name}'.")
 
-        # Extract and validate job
+        # Extract job
         job_data = rule_data.get("job", {})
-        required_job_fields = ["arguments", "output_destination", "log", "accounting_group", "accounting_group_user",
-                               "priority", "request_xferslots"]
-        for field in required_job_fields:
-            if field not in job_data:
-                raise ValueError(f"Missing required field '{field}' in job for rule '{rule_name}'.")
+
+        # Validate job
+        required_job_fields = [
+            "arguments",
+            "output_destination",
+            "log",
+            "accounting_group",
+            "accounting_group_user",
+            "priority",
+            "request_xferslots",
+        ]
+        for f in required_job_fields:
+            if f not in job_data:
+                raise ValueError(f"Missing required field '{f} in job for rule '{rule_name}'.")
+
+        filesystem = job_data.get("filesystem")
+        if filesystem is None:
+            filesystem = _default_filesystem
 
         return cls(
             name=params_data["name"],
@@ -112,11 +138,7 @@ class RuleConfig:
             comment=params_data["comment"],
             rsync=params_data["rsync"],
             mem=params_data["mem"],
-            input=InputConfig(
-                db=input_data["db"],
-                query=input_data["query"],
-                direct_path=input_data.get("direct_path")
-            ),
+            input=InputConfig(db=input_data["db"], query=input_data["query"], direct_path=input_data.get("direct_path")),
             job=JobConfig(
                 batch_name=job_data.get("batch_name"),  # batch_name is optional
                 arguments=job_data["arguments"],
@@ -126,6 +148,7 @@ class RuleConfig:
                 accounting_group_user=job_data["accounting_group_user"],
                 priority=job_data["priority"],
                 request_xferslots=job_data["request_xferslots"],
+                filesystem=filesystem
             ),
             mnrun=params_data.get("mnrun"),  # mnrun and mxrun are optional
             mxrun=params_data.get("mxrun"),
--- tester.py
+++ tester.py
@@ -4,36 +4,39 @@ import yaml
 import cProfile
 
 from logging.handlers import RotatingFileHandler
+from dataclasses import fields
 
-from simpleLogger import slogger, CustomFormatter, ERROR, INFO, DEBUG
+from simpleLogger import slogger, CustomFormatter, ERROR, WARN, INFO, DEBUG
 from ruleclasses import RuleConfig
 from utils import extract_numbers_to_commastring
 from utils import list_to_condition
 
 from argparsing import submission_args
-# Keep arguments and the parser global so we can access them everywhere and 
-# so submodules can add to them a la
-# https://mike.depalatis.net/blog/simplifying-argparse
-args     = None
-userargs = None
+
+# ============================================================================================
+# Format strings for run and segment numbers.  n.b. that the "rungroup" which defines the logfile and output file directory structure
+# hardcodes "08d" as the run format...  
+RUNFMT = "%08i"
+SEGFMT = "%05i"
 
 # ============================================================================================
 
 def main():
-
-    # digest arguments
+    ### digest arguments
     args = submission_args()
 
-    # better safe than sorry, we have multiple ways to signalize test mode
+    #################### Test mode? Multiple ways to turn it on
+    ### TODO: ".slurp" is outdated as a name, just using it for backward compatibility
     test_mode = ( 
             False
             or args.test_mode
             or 'testbed' in str(pathlib.Path(".").absolute()).lower()
-            or pathlib.Path(".slurp/testbed").is_file()
+            or pathlib.Path(".slurp/testbed").is_file() # deprecated
+            or pathlib.Path(".testbed").is_file()
             # or ( hasattr(rule, 'test_mode') and rule.test_mode )
         )
 
-    # Set up logging before going any further
+    #################### Set up logging before going any further
     if args.logdir:
         mylogdir=args.logdir
     elif test_mode:
@@ -53,6 +56,8 @@ def main():
     RotFileHandler.setFormatter(CustomFormatter())
     slogger.addHandler(RotFileHandler)
     slogger.setLevel(args.loglevel)
+    # stdout is already added by default
+    # If one cares, logging to stdout and file cans be at different levels and formats
 
     if test_mode:
         INFO("Running in testbed mode.")
@@ -63,7 +68,7 @@ def main():
     DEBUG(f"Logging to {mylogdir}")
     DEBUG(f"Log level: {args.loglevel}")
 
-    # Load specific rule from the given yaml file.
+    #################### Load specific rule from the given yaml file.
     try:
         all_rules = RuleConfig.from_yaml_file(args.config)
         rule = all_rules[args.rule]
@@ -77,8 +82,13 @@ def main():
 
     DEBUG("Rule configuration:")
     DEBUG(yaml.dump(rule.dict))
-    
-    # Which runs to process?
+
+    #################### Rule has steering parameters and two subclasses for input and job specifics
+    ### rule_input has a big SQL query template that needs to be filled in
+    ### from command line arguments and parameters
+    rule_input   = rule.input 
+
+    ### Which runs to process?
     if args.runlist:
         INFO(f"Processing runs from file: {args.runlist}")
         extract_numbers_to_commastring(args.runlist)
@@ -89,17 +99,88 @@ def main():
     if not run_condition:
         ERROR("No runs specified.")
         exit(1)
+    DEBUG(f"Run condition is  \"{run_condition}\"")
 
-    INFO(f"Run condition is  \"{run_condition}\"")
-
-    # Which segments to process?
-    seg_condition = list_to_condition(args.segments, "segment")
+    ### Which segments to process?
+    seg_condition = ""
     if seg_condition:
-        INFO(f"Segment condition is \"{seg_condition}\"")
+        seg_condition = list_to_condition(args.segments, "segment")
+        if "seg_condition" not in rule_input.query:
+            WARN("A segment condition specified but will be ignored by the rule.")
+        DEBUG(f"Segment condition is \"{seg_condition}\"")
     else:
-        INFO("No segment condition specified.")
-        seg_condition = ""
+        DEBUG("No segment condition specified.")
+
+    ### Limit the number of results from the query?
+    limit_condition = ""
+    if args.limit:
+        limit_condition = f"limit {args.limit}"
+        DEBUG(f"Limiting input query to {args.limit} entries.")
+
+    INFO( f"Basic constraints are \" where ... {run_condition} {seg_condition} {limit_condition}\"" )
+
+    # TODO: this is where Jason added the cursor pickup
+
+    ### Populate the input query with the above constraints
+    # rule_input is a psql template, "**" unpacks a dictionary into keyword arguments
+    # We could also hand over **locals() but that limits local variable names
+    # Safer and less opaque to do it explicitly
+    input_query  = rule_input.query.format(
+            **rule.dict()
+            , run_condition=run_condition
+            , seg_condition=seg_condition
+            , limit_condition=limit_condition
+            )
     
+    if args.printquery:
+        slogger.log(100, input_query) # 100 is the highest log level, it should always print
+        return 0
+
+    ### Rest of the rule.input parameters, i.e. database name and direct path
+    DEBUG (f"Using database {rule_input.db}")
+    if rule_input.direct_path is not None :
+        # A specified direct path may still need substitution for the physics mode
+        rule_input.direct_path = rule_input.direct_path.format( **vars( args ))
+    DEBUG (f"Using direct path {rule_input.direct_path}")
+
+    ### Rest of the rule parameters
+    # KK, DELME: params = config.get('params') is no longer a thing. Instead, RuleConfig has all the parameters
+    # Note that slurp did a lot of essentially copy/pasting from the config file into the rule object
+    # This is now done in the RuleConfig class yaml reader and only necessary updates and substitutions are done here
+
+    # KK, DELME: lfn2pfn is (used to be) an option
+    # lfn2pfn provides a mapping between physical files on disk and the corresponding lfn
+    # (i.e. the pfn with the directory path stripped off).
+    # Not using it until we have to (no currently existing rules have it) 
+
+    # KK, DELME: runlist_query is deprecated anyway
+
+    # KK, DELME: kaedama does rule.name = rule.name.format( **locals() ) here, but no names with {} are in the yaml files
+
+    if args.mangle_dstname:
+        rule.name = rule.name.replace('DST',args.mangle_dstname)            
+        WARN(f"DST name is mangled to {rule.name}")
+
+    # KK:  args.mangle_dirpath handled later
+
+    # Just format strings, RUNFMT and SEGFMT from the top
+    for base in [rule.outbase, rule.logbase]:
+        base=base.format( **globals( ) )
+
+    # Locations. KK, Delme: 
+    rule_job=rule.job
+
+    for field in fields(rule_job):
+        print(f'{field.name}: {getattr(rule_job, field.name)}')
+
+    ### Make sure ".slurp" and any custom files/directories are copied to the worker
+    rsync = rule.rsync + ",.slurp/"
+    rsync = rule.rsync + ",.testbed"
+    if args.append2rsync:
+        rsync = rsync + "," + args.append2rsync
+        DEBUG(f"Appending to rsync: {args.append2rsync}")
+
+
     
     INFO( "KTHXBYE!" )
 
