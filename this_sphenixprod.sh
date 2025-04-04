8f492b2281d158fc5f39c3c9b6cb8d8f2df8b803
sPHENIX Production Account <pinkenburg@bnl.gov>
2025-04-04 12:27:09 -0400

    Simpler PS1; change to this codebase

--- this_sphenixprod.sh
+++ this_sphenixprod.sh
@@ -1,9 +1,9 @@
 OS=$( hostnamectl | awk '/Operating System/{ print $3" "$4 }' )
-echo "Setting up SLURP for ${OS}"
+echo "Setting up sPHNIX Production for ${OS}"
 
 # This is the directory of the script (but no symlinks allowed)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "Slurp sys: ${SCRIPT_DIR}"
+echo "Using scripts in sys: ${SCRIPT_DIR}"
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n new
 export PATH=${PATH}:${HOME}/bin:./bin
@@ -16,37 +16,37 @@ if [[ $OS =~ "Alma" ]]; then
    alias pip=/opt/sphenix/core/bin/pip3.9
 fi
 
-export PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}/slurp
-export PATH=${PATH}:${SCRIPT_DIR}/bin
+#export PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}/slurp
+#export PATH=${PATH}:${SCRIPT_DIR}/bin
+export PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}
+export PATH=${PATH}:${SCRIPT_DIR}
 
 echo Using $(python --version)
 
-# parse_git_branch() {
-# #  branch=$( git -C ${SCRIPT_DIR} branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/' )
-#   branch=$( git -C ${SCRIPT_DIR} branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' )
-#   status="\e[31m" # red is bad
-#   if [ -z "$(git -C ${SCRIPT_DIR} status --porcelain)" ]; then
-#       status="\e[32m" # green is good
-#   fi
-#   pbranch=""
-#   pstatus=""
-#   if [ -e ProdFlow ]; then
-#      pbranch=$( git -C ./ProdFlow branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' )
-#      pstatus="\e[31m"
-#      if [ -z "$(git -C ./ProdFlow status --porcelain)" ]; then
-#         pstatus="\e[32m"     
-#      fi
-#   else
-#      pbranch="NONE"
-#      pstatus="\e[31m"
-#   fi
+parse_git_branch() {
+  branch=$( git -C ${SCRIPT_DIR} branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' )
+  status="\e[31m" # red is bad
+  if [ -z "$(git -C ${SCRIPT_DIR} status --porcelain)" ]; then
+      status="\e[32m" # green is good
+  fi
+  pbranch=""
+  pstatus=""
+  if [ -e ProdFlow ]; then
+     pbranch=$( git -C ./ProdFlow branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' )
+     pstatus="\e[31m"
+     if [ -z "$(git -C ./ProdFlow status --porcelain)" ]; then
+        pstatus="\e[32m"
+     fi
+  else
+     pbranch="NONE"
+     pstatus="\e[31m"
+  fi
   
-#   echo -e " ${status}[slurp:${branch}] ${pstatus}[prodflow:${pbranch}] "
-# }
+  #echo -e " ${status}[slurp:${branch}] ${pstatus}[prodflow:${pbranch}] "
+  echo -e " ${status}[slurp:${branch}] "
+}
 
-#PS1="\[\e[36m\]\u@\h \[\e[32m\]\w\[\e[33m\]\$(parse_git_branch)\[\e[0m\] $ "
-#PS1="\[\e[36m\]\u \[\e[32m\]\w \[\e[33m\] slurp:\$(parse_git_branch)\[\e[0m\] $ "
-#PS1="\[\e[36m\]\u@\h\[\e[33m\]\$(parse_git_branch)\[\e[32m\]\[\e[34m\]\n[\W/]> \[\e[0m\]"
-# PS1="\[\e[36m\]\u@\h\[\e[33m\]\$(parse_git_branch)\[\e[32m\]"
+#PS1="\[\e[36m\]\u\[\e[33m\]\$(parse_git_branch)\[\e[32m\]\[\e[34m\][\W/]> \[\e[0m\]"
 PS1="\[\e[36m\]me@\h\[\e[33m\]\[\e[34m\][\W/]> \[\e[0m\]"
+
 
