f299b9a5427c7c0bcca15ef42b76f16775168155
Kolja Kauder <kkauder@gmail.com>
2025-06-09 08:57:19 -0400

    Include HIST files in the spider

--- protospider.py
+++ protospider.py
@@ -94,18 +94,15 @@ def main():
     # Which find command to use?
     filesystem = rule.job_config.filesystem
 
-    # Lustre's robin hood, rbh-find, doesn't offer aadvantages but is more cumbersome to use
-    # But "lfs find" is preferrable to the regular kind. Note, it does work on gpfs so I won't bother to separate that out.
-    find = shutil.which('lfs')
-    if find is None:
+    # Lustre's robin hood, rbh-find, doesn't offer advantages for our usecase, and it is more cumbersome to use.
+    # But "lfs find" is preferrable to the regular kind.
+    lfind = shutil.which('lfs')
+    if lfind is None:
         WARN("'lfs find' not found.")
-        find = shutil.which('find')
+        lfind = shutil.which('find')
     else:
-        find = f'{find} find'
-    INFO(f"Using {find}.")
-    
-    print(filesystem['histdir'])
-    exit()
+        lfind = f'{lfind} find'
+    INFO(f'Using "{lfind}.')
     
     ##################### DSTs, from lustre to lustre
     # Original output directory, the final destination, and the file name trunk
@@ -116,7 +113,7 @@ def main():
     INFO(f"Final destination template: {finaldir}")
 
     # List of files to process
-    findcommand = f"{find} {inlocation} -type f -name {rule.rulestem}\* -print"
+    findcommand = f"{lfind} {inlocation} -type f -name {rule.rulestem}\* -print"
     INFO(f"Find command: {findcommand}")
     foundfiles = subprocess.run(findcommand, shell=True, check=True, capture_output=True).stdout.decode('utf-8').splitlines()
 
@@ -231,12 +228,126 @@ def main():
         files_curs.commit()
         datasets_curs = dbQuery( cnxn_string_map[ dbstring ], insert_datasets )
         datasets_curs.commit()
+
+    # Same thing for histogram files.
+    # TODO: Very similar, use one function for both types.
+    # Main difference is that it's easier to identify daqhost/leaf from the path
+    # TODO: Dirty hardcoding assuming knowledge of histdir naming scheme
+    find = shutil.which('find') # on gpfs, no nead for lfs find, use the more powerful generic find
+    histdir=filesystem['histdir']
+    INFO(f"Histogram directory template: {histdir}")
+    
+    # # All leafs:
+    leafparent=histdir.split('/{leafdir}')[0]
+    INFO(f"Leaf directories: \n{leafparent}")
+
+    leaffind = f"{find} {leafparent} -type d -mindepth 1 -a -maxdepth 1 -print"
+    INFO(f"Find command: {leaffind}")
+    leafdirs = subprocess.run(leaffind, shell=True, check=True, capture_output=True).stdout.decode('utf-8').splitlines()
+    CHATTY(f"Leaf directories: \n{leafdirs}")
+    
+    allhistdirs = []
+    for leafdir in leafdirs :
+        hist_find = f"{find} {leafdir} -name hist -type d -print"
+        CHATTY(f"hist directory find command: {hist_find}")
+        allhistdirs += subprocess.run(hist_find, shell=True, check=True, capture_output=True).stdout.decode('utf-8').splitlines()
+    CHATTY(f"hist directories: \n{allhistdirs}")
+
+    # Finally, run over all HIST files in those directories
+    foundfiles=[]
+    for hdir in allhistdirs:        
+        findcommand = f"{find} {hdir} -type f -name HIST\* -print"
+        tmpfound = subprocess.run(findcommand, shell=True, check=True, capture_output=True).stdout.decode('utf-8').splitlines()
+        # Remove ".root" files, they're already processed.
+        foundfiles += [ file for file in tmpfound if not file.endswith(".root") ]
+
+    tstart = datetime.now()
+    tlast = tstart
+    when2blurb=2000
+    for f, file in enumerate(foundfiles):
+        if f%when2blurb == 0:
+            now = datetime.now()
+            print( f'HIST #{f}/{len(foundfiles)}, time since previous output:\t {(now - tlast).total_seconds():.2f} seconds ({when2blurb/(now - tlast).total_seconds():.2f} Hz). ' )
+            print( f'                  time since the start      :\t {(now - tstart).total_seconds():.2f} seconds (cum. {f/(now - tstart).total_seconds():.2f} Hz). ' )
+            tlast = now
+            
+        try:
+            fullpath,_,nevents,_,first,_,last,_,md5,_,dbid = file.split(':')
+        except Exception as e:
+            WARN(f"Error: {e}")
+            continue
+
+        # Extract runnumber from the file name
+        # Note: I don't love this. It assumes a rigid file name format, and it eats time for every file.
+        #       All those splits are pretty unreadable too. Well, still beats a regex.
+        #       At least processing time turns out not to be a problem.
+        # Logic: first split is at new_nocdbtag_v000, second split isolates the run number, which is between two dashes
+        lfn=Path(fullpath).name
+        dsttype=lfn.split(f'_{rule.dataset}')[0]
+        run = int(lfn.split(rule.dataset)[1].split('-')[1])
+        # Between the dash and .root is the segment, used for the db
+        segment = int(lfn.split(rule.dataset)[1].split('-')[2].split('.root')[0])
+        
+        # --- Extract what we need for the databases
+        # for "files"
+        full_host_name = "lustre" if 'lustre' in histdir else 'gpfs'
+        full_file_path = fullpath # f'{finaldir}/{lfn}'
+        filestat=Path(file).stat()
+        ctimestamp = filestat.st_ctime
+        ctimestamp = datetime.fromtimestamp(ctimestamp)
+        file_size_bytes = filestat.st_size
+        files_table='test_files' if test_mode else 'files'
+        insert_files=f"""
+    insert into {files_table} (lfn,full_host_name,full_file_path,time,size,md5) 
+    values ('{lfn}','{full_host_name}','{full_file_path}','{ctimestamp}',{file_size_bytes},'{md5}')
+    on conflict
+    on constraint {files_table}_pkey
+    do update set 
+    time=EXCLUDED.time,
+    size=EXCLUDED.size,
+    md5=EXCLUDED.md5
+    ;
+    """
+        CHATTY(insert_files)
+
+        # for 'datasets'
+        datasets_table='test_datasets' if test_mode else 'datasets'
+        insert_datasets=f"""
+    insert into {datasets_table} (filename,runnumber,segment,size,dataset,dsttype,events)
+    values ('{lfn}',{run},{segment},{file_size_bytes},'{rule.dataset}','{dsttype}',{nevents})
+    on conflict
+    on constraint {datasets_table}_pkey
+    do update set
+    runnumber=EXCLUDED.runnumber,
+    segment=EXCLUDED.segment,
+    size=EXCLUDED.size,
+    dsttype=EXCLUDED.dsttype,
+    events=EXCLUDED.events
+    ;
+    """
+        CHATTY(insert_datasets)
+        if args.dryrun:
+            if f%when2blurb == 0:
+                print( f"mv {file} {full_file_path}" )
+            continue
+        
+        try:
+            shutil.move( file, full_file_path )
+        except Exception as e:
+            WARN(e)
+        #Register the file
+        dbstring = 'testw' if test_mode else 'fcw'
+        files_curs = dbQuery( cnxn_string_map[ dbstring ], insert_files )
+        files_curs.commit()
+        datasets_curs = dbQuery( cnxn_string_map[ dbstring ], insert_datasets )
+        datasets_curs.commit()
+
         
 # ============================================================================================
 
 if __name__ == '__main__':
-    main()
-    exit(0)
+    # main()
+    # exit(0)
 
     cProfile.run('main()', '/tmp/sphenixprod.prof')
     import pstats
