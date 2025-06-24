21cbc8623bd64eb582f46cfefbf03d91e63de35d
Kolja Kauder <kkauder@gmail.com>
2025-06-24 17:46:56 -0400

    cleanup

--- sphenixjobdicts.py
+++ sphenixjobdicts.py
@@ -10,13 +10,6 @@ inputs_from_output={
                         |   {  'ebdc39'        :  'ebdc39' }
 }
 
-# inputs_from_output={
-#     'DST_STREAMING_EVENT' : { f'INTT{n}'    : f'intt{n}'    for n in range(0,8) }
-#                         |   { f'MVTX{n}'    : f'mvtx{n}'    for n in range(0,6) }
-#                         |   { f'TPC{n:02}'  : f'ebdc{n:02}' for n in range(0,24) }
-#                         |   {  'TPOT'       :  'ebdc39' }
-# }
-
 # Clusters and seeds: many-to-1
 inputs_from_output['DST_TRKR_CLUSTER'] = list('DST_STREAMING_EVENT_' + LEAF for LEAF in inputs_from_output['DST_STREAMING_EVENT'].keys())
 inputs_from_output['DST_TRKR_SEED']    = ['DST_TRKR_CLUSTER']
@@ -26,16 +19,6 @@ inputs_from_output['DST_TRKR_TRACKS']  = ['DST_TRKR_CLUSTER','DST_TRKR_SEED']
 
 ### Calorimeters -
 # Physical detectors are triggered
-# Trigger file is %GL1%.evt; input files are /bbox%/{cosmics|physics|}'%emcal%.prdf', '%HCal%.prdf', '%LL1%.prdf', '%mbd%.prdf', '%ZDC%.prdf'
-# many-to-1
-# inputs_from_output['DST_TRIGGERED_EVENT'] = [ 'GL1%.evt'
-#                                            , 'emcal%.prdf'
-#                                            , 'HCal%.prdf'
-#                                            , 'LL1%.prdf'
-#                                            , 'mbd%.prdf'
-#                                            , 'ZDC%.prdf'
-#                                            ]
-# inputs_from_output['DST_TRIGGERED_EVENT'] = [ f'seb{n:02}' for n in range(0,24) ]
 # Keeping it as a dictionary until otherwise specified
 inputs_from_output['DST_TRIGGERED_EVENT'] = { f'seb{n:02}' : f'seb{n:02}' for n in range(0,24) }
 
