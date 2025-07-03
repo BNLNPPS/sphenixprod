dfbe61a1ba62bd994fbad591bf12215475245897
Kolja Kauder <kkauder@gmail.com>
2025-07-03 11:18:43 -0400

    Get size and ctime on the node

--- stageout.sh
+++ stageout.sh
@@ -9,7 +9,7 @@ if [ "$#" -lt "$MIN_ARG_COUNT" ] || [ "$#" -gt "$MAX_ARG_COUNT" ] ; then
     exit 0
 fi
 
-filename=${1} #filename=`basename ${1}`   # must be a local file
+filename=${1}
 destination=${2}
 dbid=${3:--1} # dbid for faster db lookup, -1 means no dbid
 if [ $dbid -eq -1 ] || [ $dbid -eq 0 ]; then
@@ -28,12 +28,6 @@ fi
 # Could test the destination like this, but we want to minimize lustre probing in the worker jobs
 # if [ -d "$destination" ]; then   echo "$DIRECTORY does exist." ; else exit 1; fi
 
-# Number of events and first/last event numbers
-# numbers=$( root.exe -l -q -b GetEntries.C\(\"${filename}\"\) 2>/dev/null | awk '/Number of Entries|First event number|Last event number/{ print $4; }' )
-# echo $nevents
-# nevents=$(echo $numbers | cut -d' ' -f1 )
-# first=$(echo $numbers | cut -d' ' -f2 )
-# last=$(echo $numbers | cut -d' ' -f3 ) 
 # Number of events and first/last event numbers
 rm -f numbers.txt
 root.exe -l -b -q GetNumbers.C\(\"${filename}\"\) 2>&1
@@ -57,12 +51,19 @@ rm -f numbers.txt cleannumbers.txt
 # md5sum:
 md5=`/usr/bin/env md5sum ${filename} | cut -d ' ' -f 1`
 
+# size and ctime
+#stat -c '%s %Y'
+size=`stat -c '%s' ${filename}`
+ctime=`stat -c '%Y' ${filename}`
+
 #change the destination filename
 destname=`basename ${filename}`
 destname="${destname}:nevents:${nevents}"
 destname="${destname}:first:${first}"
 destname="${destname}:last:${last}"
 destname="${destname}:md5:${md5}"
+destname="${destname}:size:${size}"
+destname="${destname}:ctime:${ctime}"
 destname="${destname}:dbid:${dbid}"
 
 action="cp -v ${filename} ${destination}/${destname} && rm -v ${filename}"
@@ -70,24 +71,4 @@ echo ${action}
 echo
 eval ${action}
 
-exit 0 # Fom Jason: stageout should never propagate a failed error code...
-
-# # spider can pick this back up with
-# gotname=`echo ${destname} | cut -d ':' -f 1`
-# gotnev=`echo ${destname} | cut -d ':' -f 3`
-# gotmd5=`echo ${destname} | cut -d ':' -f 5`
-# gotdbid=`echo ${destname} | cut -d ':' -f 7`
-# echo "destname        : ${destname}"
-# echo "gotname         : ${gotname}"
-# echo "destination dir : $destination"
-# echo "gotnev          : $gotnev"
-# echo "gotmd           : $gotmd5"
-# echo "gotdbid         : $gotdbid"
-
-
-
-# echo stageout ${filename} ${destination} finish `date`
-
-
-
-
+exit 0 # Fom Jason: stageout should never propagate a failed error code
