396b3bf25dd48075f4e4bdc7c0f93437721df936
Kolja Kauder <kkauder@gmail.com>
2025-07-07 14:19:12 -0400

    not working; just some snippets for reuse

--- special/bugfixer.py
+++ special/bugfixer.py
@@ -41,32 +41,45 @@ def my_parse_spiderstuff(filename: str) -> Tuple[str,...] :
     return lfn,-1,-1,-1,-1,-1
 
 # ============================================================================
-def my_parse_lfn(lfn: str, dataset:str ):
+def my_parse_lfn(lfn: str, outtriplet:str ):
     try:
-        dsttype,runsegend=lfn.split(dataset) # 'DST_..._run3auau', '-00066582-00000.root' (or .finished)
+        name=lfn.split(':')[0]
+        name=Path(name).name # could throw an error instead if we're handed a full path.
+         #  split at, and remove, run3auau_new_nocbdtag_v001, remainder is 'DST_...', '-00066582-00000.root' (or .finished)
+        dsttype,runsegend=name.split(f'_run3auau_{outtriplet}')
         _,run,segend=runsegend.split('-')
         seg,end=segend.split('.')
     except ValueError as e:
         print(f"[parse_lfn] Caught error {e}")
         print(f"lfn = {lfn}")
-        print(f"lfn.split(':') = {lfn.split(':')}")
-        print(f"name = {lfn.split(':')[0]}")
-        name=lfn.split(':')[0]
-        print(f"dsttype,runsegend = name.split(rule.dataset) = {name.split(rule.dataset)}")        
-        dsttype,runsegend=name.split(rule.dataset) # 'DST_..._run3auau', '-00066582-00000.root' (or .finished)
-        print(f"_,run,segend = runsegend.split('-') = {runsegend.split('-')}")
-        _,run,segend=runsegend.split('-')
-        print(f"seg,end = segend.split('.') = {segend.split('.')})")
-        seg,end=segend.split('.')
-        exit(-1)
+        exit(-1)        
+    return dsttype,int(run),int(seg),end
+
+# try:
+#         dsttype,runsegend=lfn.split(dataset) # 'DST_..._run3auau', '-00066582-00000.root' (or .finished)
+#         _,run,segend=runsegend.split('-')
+#         seg,end=segend.split('.')
+#     except ValueError as e:
+#         print(f"[parse_lfn] Caught error {e}")
+#         print(f"lfn = {lfn}")
+#         print(f"lfn.split(':') = {lfn.split(':')}")
+#         print(f"name = {lfn.split(':')[0]}")
+#         name=lfn.split(':')[0]
+#         print(f"dsttype,runsegend = name.split(rule.dataset) = {name.split(rule.dataset)}")        
+#         dsttype,runsegend=name.split(rule.dataset) # 'DST_..._run3auau', '-00066582-00000.root' (or .finished)
+#         print(f"_,run,segend = runsegend.split('-') = {runsegend.split('-')}")
+#         _,run,segend=runsegend.split('-')
+#         print(f"seg,end = segend.split('.') = {segend.split('.')})")
+#         seg,end=segend.split('.')
+#         exit(-1)
         
 
-    # "dsttype" as currently used in the datasets table is e.g. DST_STREAMING_EVENT_ebdc01_1_run3auau
-    # We almost have that but need to strip off a trailing "_"
-    if dsttype[-1] == '_':
-        dsttype=dsttype[0:-1]
+#     # "dsttype" as currently used in the datasets table is e.g. DST_STREAMING_EVENT_ebdc01_1_run3auau
+#     # We almost have that but need to strip off a trailing "_"
+#     if dsttype[-1] == '_':
+#         dsttype=dsttype[0:-1]
 
-    return dsttype,int(run),int(seg),end
+#    return dsttype,int(run),int(seg),end
 
 # ============================================================================================
 
@@ -80,10 +93,9 @@ where lfn='{lfn}'
 # ============================================================================================
 
 def main():
-    wrongdir='/sphenix/u/sphnxpro/mainkolja'
     rungroup_tmpl = "run_{a:08d}_{b:08d}"
-
-    prod='line_laser'
+    
+    prod='physics'
     if prod=='physics':
         prodname='run3auau'
     elif prod=='cosmics':
@@ -93,65 +105,80 @@ def main():
     else:
         print("don't know that prod")
         exit(-1)
-    pattern=f'HIST\*{prodname}\*'
-    tmpfound = shell_command(f"find {wrongdir} -type f -name {pattern}")
+
+    # type='DST_TRIGGERED'
+    type='DST_STREAMING'
+    outtriplet='new_nocdbtag_v001'
+    if 'TRKR' in type:
+        outtriplet='new_newcdbtag_v001'
+    wrongdir=f'/sphenix/lustre01/sphnxpro/production/run3auau/{prod}/{outtriplet}'
+    print(wrongdir)
+    exit()
+    
+    dpattern=f'{type}_\*'
+    # tmpfound = shell_command(f"find {wrongdir} -type f -name {pattern}")
+    cmd=f'find {wrongdir} -maxdepth 1 -type d -name {dpattern}'
+    tdirs=shell_command(cmd)
+    for tdir in tdirs:
+        cmd=f'find {tdir} -maxdepth 1 -type d -name run_\*'
+        rdirs=shell_command(cmd)
+        for rdir in rdirs:
+            print('--- Working on {rdir}')
+            chunksize=10000
+            chunkcmd = f'/usr/bin/ls -1 {rdir} | grep -v :: | head -n {chunksize}'
+            while True:
+                chunk=shell_command(chunkcmd)
+                if len(chunk)==0:
+                    break
+                for lfn in chunk:
+                    dsttype,run,seg,_=my_parse_lfn(lfn,outtriplet=outtriplet)
+                    leaf=dsttype.split(f"_{prodname}")[0]
+                    rungroup=rungroup_tmpl.format(a=100*math.floor(run/100), b=100*math.ceil((run+1)/100))
+                    #rightdir= 
+                    
+    exit()
+    
     foundhists = [ file for file in tmpfound ]
     print(f"Found {len(foundhists)} histograms to register and move.")
-
-    tstart = datetime.now()
-    tlast = tstart
-    when2blurb=2000
-    fmax=len(foundhists)
-    for f, file in enumerate(foundhists):
-        if f%when2blurb == 0:
-            now = datetime.now()
-            print( f'HIST #{f}/{fmax}, time since previous output:\t {(now - tlast).total_seconds():.2f} seconds ({when2blurb/(now - tlast).total_seconds():.2f} Hz). ' )
-            print( f'                  time since the start      :\t {(now - tstart).total_seconds():.2f} seconds (cum. {f/(now - tstart).total_seconds():.2f} Hz). ' )
-            tlast = now            
-        try:
-            lfn,nevents,first,last,md5,dbid = my_parse_spiderstuff(file)
-        except Exception as e:
-            print(f"Error: {e}")
-            continue
-        dsttype,run,seg,_=my_parse_lfn(lfn,dataset='new_nocdbtag_v001')
-        leaf=dsttype.split(f"_{prodname}")[0]
-        leaf=leaf.split("HIST_")[1]
-        rungroup=rungroup_tmpl.format(a=100*math.floor(run/100), b=100*math.ceil((run+1)/100))
-        rightdir=f'/sphenix/data/data02/sphnxpro/production/run3auau/{prod}/new_nocdbtag_v001/{leaf}/{rungroup}/hist'
-        fullpath=rightdir+'/'+lfn
+        # dsttype,run,seg,_=my_parse_lfn(lfn,dataset='new_nocdbtag_v001')
+        # leaf=dsttype.split(f"_{prodname}")[0]
+        # leaf=leaf.split("HIST_")[1]
+        # rungroup=rungroup_tmpl.format(a=100*math.floor(run/100), b=100*math.ceil((run+1)/100))
+        # rightdir=f'/sphenix/data/data02/sphnxpro/production/run3auau/{prod}/new_nocdbtag_v001/{leaf}/{rungroup}/hist'
+        # fullpath=rightdir+'/'+lfn
  
-        ### For additional db info. Note: stat is costly. Could be omitted with filestat=None
-        # Do it before the mv.
-        filestat=Path(file).stat()
+        # ### For additional db info. Note: stat is costly. Could be omitted with filestat=None
+        # # Do it before the mv.
+        # filestat=Path(file).stat()
 
-        ### Extract what else we need for file databases
-        full_file_path = fullpath
+        # ### Extract what else we need for file databases
+        # full_file_path = fullpath
 
-        dryrun=False
-        dbstring = 'fcw'
-        ### Move
-        if dryrun:
-            if f%when2blurb == 0:
-                print( f"Dryrun: Pretending to do:\n mv {file} {full_file_path}" )
-        else:           
-            # Move (rename) the file
-            try:
-                shutil.move( file, full_file_path )
-            except Exception as e:
-                WARN(e)
+        # dryrun=False
+        # dbstring = 'fcw'
+        # ### Move
+        # if dryrun:
+        #     if f%when2blurb == 0:
+        #         print( f"Dryrun: Pretending to do:\n mv {file} {full_file_path}" )
+        # else:           
+        #     # Move (rename) the file
+        #     try:
+        #         shutil.move( file, full_file_path )
+        #     except Exception as e:
+        #         WARN(e)
 
         
-        ### ... and update files catalog table
-        update_files=update_files_tmpl.format(
-            full_file_path=full_file_path,
-            lfn=lfn
-            )
-        dbstring
-        if not dryrun:
-            files_curs = dbQuery( cnxn_string_map[ dbstring ], update_files )
-            files_curs.commit()
-        else:
-            print(update_files)
+        # ### ... and update files catalog table
+        # update_files=update_files_tmpl.format(
+        #     full_file_path=full_file_path,
+        #     lfn=lfn
+        #     )
+        # dbstring
+        # if not dryrun:
+        #     files_curs = dbQuery( cnxn_string_map[ dbstring ], update_files )
+        #     files_curs.commit()
+        # else:
+        #     print(update_files)
     
 # ============================================================================================
 
