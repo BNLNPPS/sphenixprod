21d3d44bd978b77ed853db37d3358e61c0c3edaa
Kolja Kauder <kkauder@gmail.com>
2025-07-22 16:44:50 -0400

    Dedicated submission process

---
+++ execute_condorsubmission.py
@@ -0,0 +1,172 @@
+#!/bin/env python
+
+from pathlib import Path
+import cProfile
+import pstats
+import subprocess
+import sys
+import re
+from typing import List
+
+import pprint # noqa F401
+
+import argparse
+from argparsing import submission_args
+from sphenixmisc import setup_rot_handler, should_I_quit
+from simpleLogger import slogger, CustomFormatter, CHATTY, DEBUG, INFO, WARN, ERROR, CRITICAL  # noqa: F401
+from sphenixprodrules import RuleConfig
+from sphenixdbutils import test_mode as dbutils_test_mode
+from sphenixdbutils import cnxn_string_map, dbQuery
+
+# ============================================================================================
+def shell_command(command: str) -> List[str]:
+    """Minimal wrapper to hide away subbprocess tedium"""
+    CHATTY(f"[shell_command] Command: {command}")
+    ret=[]
+    try:
+        ret = subprocess.run(command, shell=True, check=True, capture_output=True).stdout.decode('utf-8').split()
+    except subprocess.CalledProcessError as e:
+        WARN("[shell_command] Command failed with exit code:", e.returncode)
+    finally:
+        pass
+
+    CHATTY(f"[shell_command] Return value length is {len(ret)}.")
+    return ret
+
+# ============================================================================================
+
+def execute_submission(rule: RuleConfig, args: argparse.Namespace):
+    """ Look for job files and submit condor jobs if the current load is acceptable.
+    Update production database to "submitted".
+    Locking and deleting is used to avoid double-submission.
+    """
+
+    ### Locate files.
+    submitdir = Path(f'{args.submitdir}').resolve()
+    subbase = f'{rule.dsttype}_{rule.dataset}_{rule.outtriplet}'
+    INFO(f'Submission files located in {submitdir}')
+    INFO(f'Submission files based on {subbase}')
+
+    sub_files = list(Path(submitdir).glob(f'{subbase}*.sub'))
+    sub_files = map(str,sub_files)
+    CHATTY(f"Submission files before run constraint:\n{pprint.pformat(sub_files)}")
+    runlist=list(map(str,rule.runlist_int))
+
+    # Only use those who match the run condition - the pythonic way
+    sub_files = {file for file in sub_files if any( f'_{runnumber}' in file for runnumber in runlist) }
+    sub_files = sorted(sub_files,reverse=True) # latest runs first
+    DEBUG(f"Submission files after run constraint:\n{pprint.pformat(sub_files)}")
+    if sub_files == []:
+        INFO("No submission files found.")
+        
+    for sub_file in sub_files:
+        in_file=re.sub(r".sub$",".in",str(sub_file))
+        ### Update production database
+        # Extract dbids
+        dbids=[]
+        try: 
+            with open(in_file,'r') as f:
+                for line in f:
+                    dbids.append(str(line.strip().split(" ")[-1]))
+        except Exception as e:
+            ERROR(f"Error while parsing {in_file}:\n{e}")
+            exit(1)
+        dbids_str=", ".join(dbids)
+        update_prod_state = f"""
+UPDATE production_status
+   SET status='submitted'
+WHERE id in
+( {dbids_str} )
+;
+""" 
+        INFO(f"Updating db for {sub_file}")
+        CHATTY(f"{update_prod_state}")
+        prod_curs = dbQuery( cnxn_string_map['statw'], update_prod_state )
+        prod_curs.commit()
+
+        INFO(f"Submitting {sub_file}\n\t\t && Removing {in_file}")
+        if not args.dryrun:
+            subprocess.run(f"condor_submit {sub_file} && rm {sub_file} {in_file}",shell=True)
+            # subprocess.run(f"echo condor_submit {sub_file} && echo rm {sub_file} {in_file}",shell=True)
+    
+    
+# ============================================================================================
+def main():
+    ### digest arguments
+    args = submission_args()
+
+    #################### Test mode?
+    test_mode = (
+            dbutils_test_mode
+            or args.test_mode
+            # or ( hasattr(rule, 'test_mode') and rule.test_mode ) ## allow in the yaml file?
+        )
+
+    # Set up submission logging before going any further
+    sublogdir=setup_rot_handler(args)
+    slogger.setLevel(args.loglevel)
+    
+    # Exit without fuss if we are already running 
+    if should_I_quit(args=args, myname=sys.argv[0]):
+        DEBUG("Stop.")
+        exit(0)
+    INFO(f"Logging to {sublogdir}, level {args.loglevel}")
+
+    if args.profile:
+        DEBUG(f"Profiling is ENABLED.")
+        profiler = cProfile.Profile()
+        profiler.enable()    
+    
+    if test_mode:
+        INFO("Running in testbed mode.")
+        args.mangle_dirpath = 'production-testbed'
+    else:
+        INFO("Running in production mode.")
+
+    #################### Rule has steering parameters and two subclasses for input and job specifics
+    # Rule is instantiated via the yaml reader.
+
+    ### Parse command line arguments into a substitution dictionary
+    # This dictionary is passed to the ctor to override/customize yaml file parameters
+    # Note: The following could all be hidden away in the RuleConfig ctor
+    # but this way, CLI arguments are used by the function that received them and
+    # constraint constructions are visibly handled away from the RuleConfig class
+    rule_substitutions = {}
+    rule_substitutions["runs"]=args.runs
+    rule_substitutions["runlist"]=args.runlist
+    rule_substitutions["prodmode"] = None  # Not relevant, but needed for the RuleConfig ctor
+    rule_substitutions["nevents"] = 0 # Not relevant, but needed for the RuleConfig ctor
+    
+    CHATTY(f"Rule substitutions: {rule_substitutions}")
+    INFO("Now loading and building rule configuration.")
+
+    #################### Load specific rule from the given yaml file.
+    try:
+        rule =  RuleConfig.from_yaml_file( yaml_file=args.config, rule_name=args.rulename, rule_substitutions=rule_substitutions )
+        INFO(f"Successfully loaded rule configuration: {args.rulename}")
+    except (ValueError, FileNotFoundError) as e:
+        ERROR(f"Error: {e}")
+        exit(1)
+
+    # CHATTY("Rule configuration:")
+    # CHATTY(yaml.dump(rule.dict))
+    
+    filesystem = rule.job_config.filesystem
+    DEBUG(f"Filesystem: {filesystem}")    
+
+    ### And go
+    execute_submission(rule, args)
+    
+    if args.profile:
+        profiler.disable()
+        DEBUG("Profiling finished. Printing stats...")
+        stats = pstats.Stats(profiler)
+        stats.strip_dirs().sort_stats('time').print_stats(10)
+
+    INFO(f"{Path(sys.argv[0]).name} DONE.")
+        
+# ============================================================================================
+
+if __name__ == '__main__':
+    main()
+    exit(0)
