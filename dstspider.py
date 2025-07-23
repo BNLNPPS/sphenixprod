d444f51031ffdb36a8278508f10516fc3f364166
Kolja Kauder <kkauder@gmail.com>
2025-07-23 10:33:42 -0400

    simplification

--- dstspider.py
+++ dstspider.py
@@ -5,12 +5,10 @@ from datetime import datetime
 import yaml
 import cProfile
 import pstats
-import subprocess
 import sys
 import shutil
 import os
 import math
-from typing import List
 
 # from dataclasses import fields
 import pprint # noqa F401
@@ -22,25 +20,10 @@ from sphenixprodrules import RuleConfig,inputs_from_output
 from sphenixprodrules import parse_lfn,parse_spiderstuff
 from sphenixdbutils import test_mode as dbutils_test_mode
 from sphenixdbutils import long_filedb_info, filedb_info, full_db_info, upsert_filecatalog, update_proddb  # noqa: F401
-from sphenixmisc import binary_contains_bisect
+from sphenixmisc import binary_contains_bisect,shell_command
+
 
 # ============================================================================================
-def shell_command(command: str) -> List[str]:
-    """Minimal wrapper to hide away subbprocess tedium"""
-    CHATTY(f"[shell_command] Command: {command}")
-    ret=[]
-    try:
-        ret = subprocess.run(command, shell=True, check=True, capture_output=True).stdout.decode('utf-8').split()
-    except subprocess.CalledProcessError as e:
-        WARN("[shell_command] Command failed with exit code:", e.returncode)
-    finally:
-        pass
-
-    CHATTY(f"[shell_command] Return value length is {len(ret)}.")
-    return ret
-
-# ============================================================================================
-
 def main():
     ### digest arguments
     args = submission_args()
