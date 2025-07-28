c0d7609f82019a0a1511eb3bfab31bc939fba387
Kolja Kauder <kkauder@gmail.com>
2025-07-28 11:48:36 -0400

    Use the same script for full and lfn lists

--- create_filelist_run_seg.py
+++ create_filelist_run_seg.py
@@ -1,52 +1,15 @@
 #!/usr/bin/env python3
 
-import sys
-from simpleLogger import slogger, CHATTY, DEBUG, INFO, WARN, ERROR, CRITICAL  # noqa: F401
-from sphenixdbutils import cnxn_string_map, dbQuery # type: ignore
+"""
+This script doesn't need to exist anymore. 
+It's a strict subset of create_full_filelist_run_seg
+"""
 
-def main():
-    if len(sys.argv) != 4:
-        script_name = sys.argv[0]
-        print(f"usage: {script_name} <inbase> <runnumber> <segment>")
-        sys.exit(0)
+import create_full_filelist_run_seg
+from simpleLogger import slogger, WARN
 
-    inbase = sys.argv[1]
-    runnumber_str = sys.argv[2]
-    segment_str = sys.argv[3]
-    
-    try:
-        runnumber = int(runnumber_str)
-        segment = int(segment_str)
-    except ValueError:
-        print(f"Error: runnumber '{runnumber_str}' must be an integer.")
-        print(f"     : segment '{segment_str}' must be an integer.")
-        sys.exit(1)
+ if __name__ == "__main__":
+    slogger.setLevel("DEBUG")
+    WARN("Deprecated. Use create_full_filelist_run_seg.py instead.")
+    create_full_filelist_run_seg.main()
 
-    # Note: dsttype isn't actually needed
-    sql_query = f"""
-    SELECT filename, dsttype 
-    FROM datasets 
-    WHERE dsttype in ( '{inbase}' )
-      AND runnumber = {runnumber}
-      AND segment ={segment}
-    ORDER BY filename
-    """
-    rows = dbQuery( cnxn_string_map['fcr'], sql_query).fetchall()
-    file_list=[]
-    for row in rows:
-        filename, dsttype = row
-        file_list.append(filename)
-
-    if not file_list:
-        print("No files found for the given criteria.")
-
-    list_filename = "infile.list"
-    try:
-        with open(list_filename, 'w') as f_out:
-            for fname in file_list:
-                f_out.write(f"{fname}\n")
-    except IOError as e:
-            print(f"Error writing to file {list_filename}: {e}")
-
-if __name__ == "__main__":
-    main()
--- create_full_filelist_run_seg.py
+++ create_full_filelist_run_seg.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 
+"""
+Generate a list of files (and one with full paths) with given specifications.
+Note: It should only find one file every time.
+"""
+
 import sys
 from simpleLogger import slogger, CHATTY, DEBUG, INFO, WARN, ERROR, CRITICAL  # noqa: F401
 from sphenixdbutils import cnxn_string_map, dbQuery # type: ignore
 
 def main():
-    if len(sys.argv) != 4:
-        script_name = sys.argv[0]
-        print(f"usage: {script_name} <inbase> <runnumber> <segment>")
-        sys.exit(0)
+    slogger.setLevel("DEBUG")
+    script_name = sys.argv[0]
+    if len(sys.argv) == 4 : 
+        WARN(f"Deprecated usage of {script_name}. Please use this signature in the future:")
+        WARN(f"usage: <dataset> <tag> <dsttype> <runnumber> <segment> ")
+        dsttype = sys.argv[1]
+        runnumber_str = sys.argv[2]
+        segment_str = sys.argv[3]
+    elif len(sys.argv) == 6 :
+        dataset = sys.argv[1]
+        tag     = sys.argv[2]
+        dsttype = sys.argv[3]
+        runnumber_str = sys.argv[4]
+        segment_str = sys.argv[5]
+    else:
+        ERROR(f"usage: [dataset] [tag] [dsttype] <runnumber> <segment> ")                
+        sys.exit(1)
 
-    inbase = sys.argv[1]
-    runnumber_str = sys.argv[2]
-    segment_str = sys.argv[3]
-    
     try:
         runnumber = int(runnumber_str)
         segment = int(segment_str)
@@ -26,23 +40,31 @@ def main():
     sql_query = f"""
     SELECT datasets.filename,files.full_file_path 
     FROM files,datasets 
-    WHERE datasets.dsttype in ( '{inbase}' )
+    WHERE datasets.dsttype in ( '{dsttype}' )"""
+    #### This should _always_ be provided. Only leaving it optional for backward  compatibility
+    if len(sys.argv) == 6 :
+        sql_query += f"""
+      AND datasets.tag='{tag}'
+      AND datasets.dataset = '{dataset}' """
+    ### Rest of query
+    sql_query += f"""
       AND datasets.runnumber = {runnumber}
       AND datasets.segment ={segment}
       AND files.lfn=datasets.filename
     ORDER BY datasets.filename
-    """
+    ;"""
+    DEBUG (f"datasets query is {sql_query}")
     rows = dbQuery( cnxn_string_map['fcr'], sql_query).fetchall()
     file_list=[]
     full_path_list=[]
     for row in rows:
-        filename,full_file_path = row
-        file_list.append(filename)
-        full_path_list.append(full_file_path)
+        file_list.append(row.filename)
+        full_path_list.append(row.full_file_path)
 
     if not file_list:
         print("No files found for the given criteria.")
-
+        exit(0)
+        
     list_filename = "infile.list"
     full_path_list_filename = "infile_paths.list"
     try:
@@ -55,6 +77,7 @@ def main():
     try:
         with open(full_path_list_filename, 'w') as f_out:
             for fname in full_path_list:
+                DEBUG(f"Adding {fname}")
                 f_out.write(f"{fname}\n")
     except IOError as e:
             print(f"Error writing to file {full_path_list_filename}: {e}")
