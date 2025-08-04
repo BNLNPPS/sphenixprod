a74afd0e60f969013880d6f52d3e090dae4f7f83
Kolja Kauder <kkauder@gmail.com>
2025-08-04 14:26:13 -0400

    fixes amd tweaks

--- create_full_filelist_run_seg.py
+++ create_full_filelist_run_seg.py
@@ -14,18 +14,18 @@ def main():
     script_name = sys.argv[0]
     if len(sys.argv) == 4 : 
         WARN(f"Deprecated usage of {script_name}. Please use this signature in the future:")
-        WARN(f"usage: <dataset> <tag> <dsttype> <runnumber> <segment> ")
+        WARN(f"usage: <dataset> <intriplet> <dsttype> <runnumber> <segment> ")
         dsttype = sys.argv[1]
         runnumber_str = sys.argv[2]
         segment_str = sys.argv[3]
     elif len(sys.argv) == 6 :
         dataset = sys.argv[1]
-        tag     = sys.argv[2]
+        intriplet = sys.argv[2]
         dsttype = sys.argv[3]
         runnumber_str = sys.argv[4]
         segment_str = sys.argv[5]
     else:
-        ERROR(f"usage: [dataset] [tag] [dsttype] <runnumber> <segment> ")                
+        ERROR(f"usage: [dataset] [intriplet] [dsttype] <runnumber> <segment> ")                
         sys.exit(1)
 
     try:
@@ -36,34 +36,54 @@ def main():
         print(f"     : segment '{segment_str}' must be an integer.")
         sys.exit(1)
 
-    # Note: dsttype isn't actually needed
-    sql_query = f"""
-    SELECT datasets.filename,files.full_file_path 
-    FROM files,datasets 
-    WHERE datasets.dsttype in ( '{dsttype}' )"""
+    # dsttype comes as a a comma-separated list. Add ticks and parens
+    dsttype4sql=dsttype.replace(",","','")    
+    
+    # Why not the following, you ask?
+    # SELECT datasets.filename,files.full_file_path 
+    # FROM files,datasets 
+    # WHERE files.lfn=datasets.filename
+    # Because it's very slow. So split it into separate queries.
+    datasets_query = f"""
+    SELECT filename
+    FROM datasets 
+    WHERE datasets.dsttype in ( '{dsttype4sql}' )
+    AND datasets.runnumber = {runnumber}
+    AND datasets.segment = {segment} """
     #### This should _always_ be provided. Only leaving it optional for backward  compatibility
     if len(sys.argv) == 6 :
-        sql_query += f"""
-      AND datasets.tag='{tag}'
-      AND datasets.dataset = '{dataset}' """
-    ### Rest of query
-    sql_query += f"""
-      AND datasets.runnumber = {runnumber}
-      AND datasets.segment ={segment}
-      AND files.lfn=datasets.filename
-    ORDER BY datasets.filename
-    ;"""
-    DEBUG (f"datasets query is {sql_query}")
-    rows = dbQuery( cnxn_string_map['fcr'], sql_query).fetchall()
+        datasets_query += f"""
+      AND tag='{intriplet}'
+      AND dataset = '{dataset}'"""
+    datasets_query += ";"
+    
+    print (f"datasets query is {datasets_query}")
+    rows = dbQuery( cnxn_string_map['fcr'], datasets_query).fetchall()
     file_list=[]
-    full_path_list=[]
     for row in rows:
         file_list.append(row.filename)
-        full_path_list.append(row.full_file_path)
 
     if not file_list:
         print("No files found for the given criteria.")
-        exit(0)
+        exit(1)
+    filelist=sorted(file_list)
+    
+    ### Collect full paths. Note, we can make this optional for combiner jobs.
+    filelist_str="','".join(filelist)
+    files_query = f"""
+    SELECT full_file_path
+    FROM files
+    WHERE lfn in ( '{filelist_str}' )
+    ;"""
+    print (f"files query is {files_query}")
+    rows = dbQuery( cnxn_string_map['fcr'], files_query).fetchall()
+    full_path_list=[]
+    for row in rows:
+        full_path_list.append(row.full_file_path)
+
+    if not full_path_list:
+        print("No files found for the given criteria.")
+        exit(1)
         
     list_filename = "infile.list"
     full_path_list_filename = "infile_paths.list"
@@ -77,7 +97,7 @@ def main():
     try:
         with open(full_path_list_filename, 'w') as f_out:
             for fname in full_path_list:
-                DEBUG(f"Adding {fname}")
+                # print(f"Adding {fname}")
                 f_out.write(f"{fname}\n")
     except IOError as e:
             print(f"Error writing to file {full_path_list_filename}: {e}")
