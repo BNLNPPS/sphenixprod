59d5fde731203bb4725728e242f4c4c9d36fe5d0
Kolja Kauder <kkauder@gmail.com>
2025-08-05 08:43:55 -0400

    comments

--- production_control.py
+++ production_control.py
@@ -84,7 +84,8 @@ def main():
             ruleargs+=" --dryrun"
         
         ### Loop through process types
-        ### Go from fast to slow processes. They'll overlap anyway, but we may want to add
+        ### Go from fast to slow processes.
+        ### They'll overlap anyway.
         INFO(f"sdh_tuple is {pprint.pformat(sdh_tuple)}")
         if sdh_tuple.histspider:
             procline=f"histspider.py {ruleargs}"
