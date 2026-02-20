933f830756b3d997979db630cf36f10edae0eb90
Kolja Kauder <kkauder@gmail.com>
2026-02-20 13:33:43 -0500

    Experimenting with PS1

--- this_sphenixprod.sh
+++ this_sphenixprod.sh
@@ -18,24 +18,36 @@ source /opt/sphenix/core/bin/sphenix_setup.sh -n new
 export PATH=${PATH}:${HOME}/bin:./bin
 export ODBCINI=./.odbc.ini
 
-# if [[ $OS =~ "Alma" ]]; then
-#    export PATH=/usr/bin:${PATH}
-#    export PYTHONPATH=/opt/sphenix/core/lib/python3.9/site-packages
-#    alias python=/usr/bin/python
-#    alias pip=/opt/sphenix/core/bin/pip3.9
-# fi
-
 export PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}
 export PATH=${PATH}:${SCRIPT_DIR}
 
 echo Using $(python --version)
 
-# parse_git_branch() {
-#    branch=$( git -C ${SCRIPT_DIR} rev-parse --abbrev-ref HEAD  2> /dev/null )
-#    local branch_color_status="\e[31m" # red is bad
-#    if [ -z "$(git -C ${SCRIPT_DIR} status --porcelain -uno)" ]; then
-#       branch_color_status="\e[32m" # green is good
-#    fi
+parse_git_branch() {
+    #branch name
+    branch=$( git -C ${SCRIPT_DIR} rev-parse --abbrev-ref HEAD  2> /dev/null )
+
+    local branch_color_status="\e[31m" # red is dangerous
+    if [[ "$branch" == "main" ]] ; then
+        branch_color_status="\e[32m" # green is safe
+    fi
+
+    # bold font if there are uncommitted changes
+    if [ -z "$(git -C ${SCRIPT_DIR} status --porcelain -uno)" ]; then
+        : # nop
+    else
+        branch_color_status="$branch_color_status\e[1m"
+    fi
+
+    # # decide color based on whether we're in main, and whether there are uncommited changes
+    # local branch_color_status="\e[31m" # red is dangerous
+
+
+    # if [ -z "$(git -C ${SCRIPT_DIR} status --porcelain -uno)" ]; then
+    #    branch_color_status="\e[32m" # green is good
+    # fi
+
+
 #    pbranch=""
 #    pstatus=""
 #    if [ -e ProdFlow ]; then
@@ -49,16 +61,20 @@ echo Using $(python --version)
 #       pstatus="\e[31m"
 #   fi
 
-#    gitstatus="${branch_color_status} prod:${branch}\e[0m"
-#    if [ -n "$pbranch" ]; then
-#       #status="${status} \e[0;33mprodflow:${pbranch}\e[0m"
-#       gitstatus="${gitstatus} ${pstatus} prodflow:${pbranch}"
-#    fi
-#    echo -e "${gitstatus}\e[0m"
-#    #echo -e "\[${branch_color_status}\]prod:${branch}\[\e[0m\] \[${pstatus}\]prodflow:${pbranch}\[\e[0m\]"
-# }
-# PS1="\u@\h $(parse_git_branch) \W> "
-PS1="\u@\h \W> "
+    gitstatus="${branch_color_status} git:${branch}\e[0m"
+    # if [ -n "$pbranch" ]; then
+    #     #status="${status} \e[0;33mprodflow:${pbranch}\e[0m"
+    #     # gitstatus="${gitstatus} ${pstatus} prodflow:${pbranch}"
+    #     gitstatus="${gitstatus} ${pstatus}"
+    # fi
+    echo -e "${gitstatus}\e[0m"
+    #echo -e "\[${branch_color_status}\]prod:${branch}\[\e[0m\] \[${pstatus}\]prodflow:${pbranch}\[\e[0m\]"
+}
+#Could also use PROMPT_COMMAND=parse_git_branch
+#PS1="\u@\h $(parse_git_branch) \W> "
+PS1="\h:\w\$(parse_git_branch)> "
+
+# PS1="\u@\h \W> "
 
 if [[ "$-" == *i* ]]; then
    # echo "Interactive shell"
